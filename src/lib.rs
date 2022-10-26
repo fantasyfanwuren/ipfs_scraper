35ea7cc2a9557a49689f48e6f5892a312713e9d9
fantasyfanwuren <756423901@qq.com>
2022-10-26 22:41:22 +0800

    增加了详细的注释

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@ use std::fs;
 use std::io::{self, Read, Write};
 use std::path::{Path, PathBuf};
 
+/// # 该函数用于获取用户的输入
+///
+/// 首先获取用户输入的网址
+///
+/// 然后获取用户输入的线程数
+///
 pub fn get_input() -> Result<(String, i32), Box<dyn std::error::Error>> {
     // 首先获取用户输入的网址
     println!("");
@@ -28,18 +34,31 @@ pub fn get_input() -> Result<(String, i32), Box<dyn std::error::Error>> {
     Ok((url, thread_num))
 }
 
+/// # 解析从ipfs网站获取的源码信息,从中获取所有需要下载的文件名
+///
+/// 它首先会查找第一个"filename="字符串,然后找到紧随其后的"\""(引号)
+///
+/// 然后获取 两个字符串之间的内容就是文件名
+///
+/// 循环处理以上内容,得到一个只含有文件名的数组Vec<String>
+///
 pub fn analysis(code: &str) -> Vec<String> {
     let mut file_names: Vec<String> = vec![];
     let mut code = &code[..];
     loop {
+        // 首先找到第一个"filename="的位置 begin
         if let Some(begin) = code.find("filename=") {
+            // 将切片指向begin之后的位置
             code = &code[begin..];
         } else {
+            // 若无法找到,说明链接有问题,或者已经循环完毕,就退出
             break;
         }
+        // 找到"filename="后面紧跟着的引号,将其位置定义为begin
         if let Some(begin) = code.find("\"") {
+            // 截取 "filename=" 和 其后引号之间的文本,加入到要返回的Vec<String>中
             file_names.push((&code[9..begin]).to_string());
-            //println!("{}{}", url, &code[9..begin]);
+            // 将切片指向新的begin之后的内容
             code = &code[begin..]
         } else {
             break;
@@ -48,12 +67,16 @@ pub fn analysis(code: &str) -> Vec<String> {
     file_names
 }
 
+/// # 该函数以一个字符串hash作为参数,然后在当前程序所在目录下创建一个hash字符串为命名的文件夹
+///
+/// 它内部会进行一定的判断:如果文件夹已经存在就把他删除,并重新创建,如果不存在就直接创建
+///
 pub fn make_dir(hash: &str) -> Result<PathBuf, io::Error> {
     //创建文件夹
     let dir = std::env::current_dir().unwrap();
     let dir = dir.join(hash);
     let dir_path = Path::new(&dir);
-    println!("{:?}", dir_path);
+    // 判断字符串是否存在
     if dir.is_dir() {
         fs::remove_dir_all(dir_path)?;
     }
@@ -61,22 +84,33 @@ pub fn make_dir(hash: &str) -> Result<PathBuf, io::Error> {
     println!("所有输出将保存在以下目录:{:?}", dir_path);
     Ok(dir)
 }
+/// # 提供下载功能:参数为下载链接的地址和下载后需要保存的路径(这个路径是包含保存后的命名的)
+///
+/// 实现原理很简单:先下载数据,在把数据转化为可写入的类型(该类型内含错误传递机制)
+/// 写入并保存
 pub fn download(url: &str, file_path: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
-    println!("{url}");
+    // 先下载数据
     let body = reqwest::blocking::get(url)?.bytes()?;
+    // 创建一个空文件
     let mut file = match std::fs::File::create(file_path) {
         Err(why) => panic!("couldn't create {}", why),
         Ok(file) => file,
     };
+    // 将下载后的数据转化为Bytes<&[u8]>类型
     let content = body.bytes();
+    // 将数据Bytes<&[u8]>类型转化为 带有错误传递机制的Vec<[u8]>类型,这是可以写入文件的一种类型
     let data: std::result::Result<Vec<_>, _> = content.collect();
-    file.write_all(&data.unwrap())?;
+    // 将数据写入之前创建的空文件
+    file.write_all(&data?)?;
     Ok(())
 }
+/// #从目标链接获取网页的源码信息,并生成为&str类型,方便后面解析用
 pub fn get_code(url: &str) -> std::result::Result<String, Box<dyn std::error::Error>> {
     let code = reqwest::blocking::get(url)?.text()?;
     Ok(code)
 }
+// 以下是测试,因为代码都很简单,所有没有全部写测试,只测试了最拿不准的下载功能,可以运行cargo test看看效果
+// 大多数都在main函数里面测试了,但当你的程序特别大的时候,单元测试是十分必要的
 #[cfg(test)]
 mod test {
     use super::*;
--- src/main.rs
+++ src/main.rs
@@ -26,11 +26,13 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     // 创建原子引用及互斥锁
     // index 用于流程控制,没开启一个线程,则index加1
     // file_names 是线程间需要不断访问的一个数组,里面存放着需要保存的文件名,通过与url进行字符串拼接可以获得要下载的地址
+    // dir_path 也创建了锁,为了方便与file_names合成一个文件路径使用
 
     let index = Arc::new(Mutex::new(0));
     let file_names = Arc::new(Mutex::new(file_names));
     let url = Arc::new(Mutex::new(url));
     let dir_path = Arc::new(Mutex::new(dir_path));
+    // 创建一个存放线程的数组,用于在线程全部启动后,等待他们全部结束后,退出程序
     let mut handles = vec![];
     for i in 0..thread_num {
         // 得到需要进入线程的数据
@@ -45,23 +47,28 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
                 let link = url.lock().unwrap();
                 let file_names = file_names.lock().unwrap();
                 let dir_path = dir_path.lock().unwrap();
-                // 设置线程结束条件
+                // 设置线程结束条件,当发现所有的filenames被遍历完了之后,就退出,这对于每个线程都是平等的
                 if *num1 >= file_names.len() {
                     break 'a;
                 }
                 // 得到 下载链接 和 文件地址
-                let division = if link.ends_with("/") { "" } else { "/" };
-                let download_link = format!("{}{}{}", link, division, file_names[*num1]);
+                let division = if link.ends_with("/") { "" } else { "/" }; //如果链接后面有"/"就什么都不加,如果没有就加上
+                let download_link = format!("{}{}{}", link, division, file_names[*num1]); // 通过字符串的拼接,拼接出当前文件的下载链接
                 let download_file_path =
-                    format!("{}/{}", dir_path.to_str().unwrap(), file_names[*num1]);
+                    format!("{}/{}", dir_path.to_str().unwrap(), file_names[*num1]); // 通过字符串的拼接,拼接出当前文件的下载路径(含文件名)
                 *num1 += 1;
                 println!("线程{i}:{download_file_path}开始下载");
-                // 给每个进入线程的数据进行解锁,让其他线程开始启动
+                // 给每个进入线程的数据进行解锁,让其他线程开始启动,注意,一个都别漏掉,漏掉一个都会成为死锁程序
+                // 如果对多线程有任何疑问,可以参考学习一下文档后,再来看代码,绝对能看明白:
+                // 闭包: https://kaisery.github.io/trpl-zh-cn/ch13-01-closures.html
+                // 智能指针: https://kaisery.github.io/trpl-zh-cn/ch15-00-smart-pointers.html
+                // 互斥锁:https://kaisery.github.io/trpl-zh-cn/ch16-03-shared-state.html
                 drop(num1);
                 drop(link);
                 drop(file_names);
                 drop(dir_path);
-                //std::thread::sleep(std::time::Duration::from_millis(100));
+                // 这里之所以要加入循环是为了防止下载超时的错误导致线程退出,从而降低下载效率和准确性
+                // 循环可以让下载超时后,继续重新下载.这对ipfs下载十分有用,就算网络很差也不用怕
                 'b: loop {
                     match ipfs_scraper::download(&download_link, &download_file_path) {
                         Ok(_) => {
@@ -75,7 +82,8 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
         });
         handles.push(handle);
     }
-
+    // 这里是为了让主线程等待所有线程结束后再退出程序用的一个等待
+    // 可以试试删除这段代码会怎样.
     for handle in handles {
         handle.join().unwrap();
     }
